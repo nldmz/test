0c518c58ce86050fabc2dba2c539721210d7fbf1
nldmz <112802201+nldmz@users.noreply.github.com>
2023-11-26 14:15:32 +0800

    Delete ICDE24_Paper616_Code/src directory

--- ICDE24_Paper616_Code/src/followerPeel.h
+++
@@ -1,9 +0,0 @@
-#include "utility.h"
-#include "biGraph.h"
-using namespace std;
-
-
-
-void vertex_division(BiGraph &g);
-void follower_computation(BiGraph &g);
-void dp_peeling(BiGraph &g, vid_t q, int as, int bs, int layer);
--- ICDE24_Paper616_Code/src/judgeCore.h
+++
@@ -1,9 +0,0 @@
-#include "utility.h"
-#include "biGraph.h"
-using namespace std;
-
-void generateCombinations_left(int n, int m, vector<int>& combination, int start);
-void generateCombinations_right(int n, int m, vector<int>& combination, int start);
-void enumCombinations(BiGraph& g, int as, int bs, int alpha, int beta);
-void find_largest_a_b_core(BiGraph& g, int alpha, int beta);
-float is_abCore(BiGraph &g, int alpha, int beta);
